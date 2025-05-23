f0686d0cd3ca8f5c995cf8602f78b068a205cf03
Naeem <siyalnaeem1997@gmail.com>
2025-05-23 23:05:25 +0100

    Implement fund and getBalance function

---
+++ index-js.js
@@ -0,0 +1,93 @@
+import { createWalletClient, custom, createPublicClient, parseEther, defineChain, formatEther } from "https://esm.sh/viem";
+import { contractAddress, abi } from "./constants-js.js";
+
+const connectBtn = document.getElementById("connectBtn");
+const fundBtn = document.getElementById("fundBtn");
+const ethAmount = document.getElementById("ethAmount")
+const balanceBtn = document.getElementById("balanceBtn")
+
+
+let walletClient;
+let publicClient;
+
+async function connect() {
+  if(typeof window.ethereum !== 'undefined') {
+    walletClient = createWalletClient({
+      transport: custom(window.ethereum),
+    });
+
+    let addresses = await walletClient.requestAddresses();
+    connectBtn.innerHTML = "Connected";
+
+  }else{
+    connectBtn.innerHTML = "Please install MetaMask";
+  }
+}
+
+async function fund() {
+  const amount = ethAmount.value
+  console.log(`Funding with ${amount}...`)
+
+  if (typeof window.ethereum !== "undefined") {
+    walletClient = createWalletClient({
+      transport: custom(window.ethereum),
+    })
+    const [account] = await walletClient.requestAddresses()
+    const currentChain = await getCurrentChain(walletClient)
+
+    console.log("Processing transaction...")
+    publicClient = createPublicClient({
+      transport: custom(window.ethereum),
+    })
+    const { request } = await publicClient.simulateContract({
+      address: contractAddress,
+      abi,
+      functionName: "fund",
+      account,
+      chain: currentChain,
+      value: parseEther(amount),
+    })
+    console.log(request);
+    
+    const hash = await walletClient.writeContract(request)
+    console.log("Transaction processed: ", hash)
+  } else {
+    fundBtn.innerHTML = "Please install MetaMask"
+  }
+}
+
+async function getBalance() {
+  if (typeof window.ethereum !== "undefined") {
+    publicClient = createPublicClient({
+      transport: custom(window.ethereum),
+    })
+
+    const balance = await publicClient.getBalance({
+      address: contractAddress,
+    })
+    console.log("Balance: ", formatEther(balance))
+  }
+}
+
+async function getCurrentChain(client) {
+  const chainId = await client.getChainId()
+  const currentChain = defineChain({
+    id: chainId,
+    name: "Custom Chain",
+    nativeCurrency: {
+      name: "Ether",
+      symbol: "ETH",
+      decimals: 18,
+    },
+    rpcUrls: {
+      default: {
+        http: ["http://localhost:8545"],
+      },
+    },
+  })
+  return currentChain
+}
+
+connectBtn.onclick = connect;
+fundBtn.onclick = fund;
+balanceBtn.onclick = getBalance;
